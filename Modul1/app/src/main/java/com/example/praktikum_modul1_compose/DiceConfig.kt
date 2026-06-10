ed4ea8e6466b24a5f4bb243ad5cbbd5e86ce91d8
Rakha <rakha1140athallah@gmail.com>
2026-06-10 19:17:48 +0800

    push compose modul 1

---
+++ Modul1/app/src/main/java/com/example/praktikum_modul1_compose/DiceConfig.kt
@@ -0,0 +1,8 @@
+package com.example.praktikum_modul1_compose
+
+object DiceConfig {
+    const val SHUFFLE_DURATION_MILLIS = 500L
+    const val SHUFFLE_STEP_MILLIS = 50L
+    const val SHUFFLE_LOOP_COUNT = 10
+    const val ROTATION_INCREMENT = 360f
+}
